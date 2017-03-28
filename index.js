3eb6881c56b02fb7d91b5630860e5650ccd18fb8
Christian Herweg <christian.herweg@otto.de>
2017-03-28 14:04:11 +0200

    Add recieve method and implement new query strukture

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('lodash');
 var fs = require('fs');
+var url = require('url');
 var app = express();
 
-app.use(bodyParser.json());
+var rawBodySaver = function (req, res, buf, encoding) {
+  if (buf && buf.length) {
+    req.rawBody = buf.toString(encoding || 'utf8');
+  }
+}
+
+app.use(bodyParser.json({ verify: rawBodySaver }));
 
 var loadTime = new Date().getTime();
 var timeserie = require('./series.json');
@@ -56,22 +63,28 @@ app.all('/search', function(req, res){
 app.all('/annotations', function(req, res) {
   setCORSHeaders(res);
   console.log(req.url);
-  console.log(req.body);
+  console.log(req.body.annotation.query);
+  query_json = [];
+    query_json = req.body.annotation.query.split(',').reduce(function(o,pair) {
+      pair = pair.split(':');
+      return o[pair[0]] = pair[1], o;
+      }, {});   
 
   var tsResult = [];
-  _.each(req.body.annotation.query, function(query) {
-      var k = _.filter(annotations, function(q) {
-        return q.tags === query.tags;
-      });
-
-      _.each(k, function(kk) {
+  _.each(query_json, function(value, key, object) {
+    console.log(key + "..." + value);
+    var k = _.filter(annotations,function(q) {
+    var search_base = eval('q.' + key);
+    return search_base === value;
+    });
+     _.each(k, function(kk) {
         tsResult.push(kk)
-      });
+     });
   });
-
+	  
   res.json(tsResult);
   res.end();
-})
+});
 
 app.all('/query', function(req, res){
   setCORSHeaders(res);
@@ -96,6 +109,22 @@ app.all('/query', function(req, res){
 
   res.json(tsResult);
   res.end();
+});
+
+app.use('/public', express.static(__dirname + '/public'));  
+app.use(express.static(__dirname + '/public')); 
+
+app.post('/receive', function(request, respond) {
+    var body = '';
+    var ondisk_data = [];
+    filePath = __dirname + '/public/data.json';
+    console.log(filePath);
+    ondisk_data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+    ondisk_data.push(request.rawBody);
+    //console.log('THE DATA:' + JSON.stringify(ondisk_data));
+    fs.writeFile(filePath,JSON.stringify(ondisk_data), function() {
+	                respond.end();
+    });
 });
 
 app.listen(3333);
